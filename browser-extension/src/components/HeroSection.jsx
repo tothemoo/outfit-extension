888b4461d42cbefe515ab9dd9a5c8fc12e06734d
tothemoo <rengi0000@gmail.com>
2025-12-23 02:37:46 +0530

    few changes

--- browser-extension/src/components/HeroSection.jsx
+++ browser-extension/src/components/HeroSection.jsx
@@ -24,6 +24,11 @@ import { styled } from "@mui/material/styles";
 import { tableCellClasses } from "@mui/material/TableCell";
 
 const HeroSection = () => {
+
+
+
+
+  //table style
   const StyledTableCell = styled(TableCell)(({ theme }) => ({
     [`&.${tableCellClasses.head}`]: {
       backgroundColor: theme.palette.common.black,
@@ -48,90 +53,97 @@ const HeroSection = () => {
 
   return (
     <div className="section-1">
-      <Box sx={{ width: 360, minHeight: 480, p: 2, boxShadow: 10 }}>
+      <Box sx={{ width: 360, minHeight: 480, boxShadow: 10 }}>
 
-        <Stack spacing={2}>
 
-          {/* header section */}
-          <AppBar position="sticky" elevation={1} >
-            <Toolbar variant="dense">
 
-              <Typography
-                variant="subtitle1"
-                sx={{ fontWeight: 500 }}
-              >
-                Outfit Grabber
-              </Typography>
+        {/* header section */}
+        <AppBar position="sticky" elevation={1} >
+          <Toolbar variant="dense">
 
-              {/* pushes anything after this to the right */}
-              <Box sx={{ flexGrow: 1 }} />
+            <Typography
+              variant="subtitle1"
+              sx={{ fontWeight: 500 }}
+            >
+              Outfit Grabber
+            </Typography>
 
-              <Button variant="outlined" color="white" size="small">
-                <Typography sx={{ fontSize: "small" }}>New</Typography> <AddIcon sx={{ fontSize: "medium" }} />
-              </Button>
-            </Toolbar>
-          </AppBar>
+            {/* pushes anything after this to the right */}
+            <Box sx={{ flexGrow: 1 }} />
 
-          {/* link grabber */}
-          <Box sx={{
-            width: "100%",
-            minHeight: 100,
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}>
-            <Paper elevation={3} sx={{
+            <Button variant="outlined" color="white" size="small">
+              <Typography sx={{ fontSize: "small" }}>New</Typography> <AddIcon sx={{ fontSize: "medium" }} />
+            </Button>
+          </Toolbar>
+        </AppBar>
+
+
+        <Box sx={{ p: 2 }}>
+
+          <Stack spacing={2}>
+
+            {/* link grabber */}
+            <Box sx={{
               width: "100%",
-              height: "100%",
               minHeight: 100,
               display: "flex",
               alignItems: "center",
               justifyContent: "center",
+            }}>
+              <Paper elevation={3} sx={{
+                width: "100%",
+                height: "100%",
+                minHeight: 100,
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
 
-              borderRadius: 2,
-            }} >
-              <Stack direction="row" spacing={1} alignItems="center">
-                <Typography sx={{ color: "grey" }}>Drop your Link</Typography>
-                <AddLinkIcon sx={{ color: "grey" }} />
-              </Stack>
-            </Paper>
+                borderRadius: 2,
+              }} >
+                <Stack direction="row" spacing={1} alignItems="center">
+                  <Typography sx={{ color: "grey" }}>Drop your Link</Typography>
+                  <AddLinkIcon sx={{ color: "grey" }} />
+                </Stack>
+              </Paper>
 
-          </Box>
+            </Box>
 
-          {/* add Button */}
-          <Box display="flex">
-            <Button variant="outlined" endIcon={<AddIcon />} sx={{ color: "black" }}>Add</Button>
-          </Box>
+            {/* add Button */}
+            <Box display="flex" >
+              <Button variant="contained" endIcon={<AddIcon />} sx={{ color: "white" }}>Add</Button>
+            </Box>
 
 
-          {/* table section */}
-          <TableContainer component={Paper} sx={{ maxHeight: 300 }}>
-            <Table size="small" stickyHeader>
-              <TableHead>
-                <TableRow>
-                  <StyledTableCell>Sl no.</StyledTableCell>
-                  <StyledTableCell>Item</StyledTableCell>
-                  <StyledTableCell>Qty</StyledTableCell>
-                  <StyledTableCell>Price</StyledTableCell>
-                </TableRow>
-              </TableHead>
+            {/* table section */}
+            <TableContainer component={Paper} sx={{ maxHeight: 300 }}>
+              <Table size="small" stickyHeader>
+                <TableHead>
+                  <TableRow>
+                    <StyledTableCell>Sl no.</StyledTableCell>
+                    <StyledTableCell>Item</StyledTableCell>
+                    <StyledTableCell>Qty</StyledTableCell>
+                    <StyledTableCell>Price</StyledTableCell>
+                  </TableRow>
+                </TableHead>
 
-              <TableBody>
-                <StyledTableRow>
-                  <StyledTableCell>1</StyledTableCell>
-                  <StyledTableCell>Zipper Jacket</StyledTableCell>
-                  <StyledTableCell>2</StyledTableCell>
-                  <StyledTableCell>200</StyledTableCell>
-                </StyledTableRow>
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </Stack>
+                <TableBody>
+                  <StyledTableRow>
+                    <StyledTableCell>1</StyledTableCell>
+                    <StyledTableCell>Zipper Jacket</StyledTableCell>
+                    <StyledTableCell>2</StyledTableCell>
+                    <StyledTableCell>200</StyledTableCell>
+                  </StyledTableRow>
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Stack>
+        </Box>
 
 
 
       </Box>
-    </div >
+
+    </div>
   );
 };
 
